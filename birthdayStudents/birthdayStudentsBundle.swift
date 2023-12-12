33f9d7c33dce9ac097fc4692879138cf99e8ea03
2288-256 <74705442+2288-256@users.noreply.github.com>
2023-12-12 17:36:25 +0900

    誕生日を表示するウィジェットを追加

---
+++ birthdayStudents/birthdayStudentsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  birthdayStudentsBundle.swift
+//  birthdayStudents
+//
+//  Created by clark on 2023/12/12.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct birthdayStudentsBundle: WidgetBundle {
+    var body: some Widget {
+        BirthdayStudentsWidget()
+    }
+}
